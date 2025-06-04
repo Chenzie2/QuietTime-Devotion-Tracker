e4aa625bcfe9eefa410e06c0063509eb98cb41da
g_zawie <gzawie24@gmail.com>
2025-06-04 13:38:41 +0300

    Final commit

--- lib/debug.py
+++ lib/debug.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from lib.models import Base  
 
-engine = create_engine("sqlite:///quiettime_devotion_tracker.db", echo=False)
+engine = create_engine("sqlite:///quiettime_devotion_tracker.db", echo=True)
 
 Base.metadata.create_all(engine)
 
